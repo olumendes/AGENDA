800c2eed9bea8b2ca0887cb3889e2044721786d1
olumendes <123881011+olumendes@users.noreply.github.com>
2026-04-17 11:13:34 -0300

    Create configuration header file config.h

---
+++ config.h
@@ -0,0 +1,11 @@
+// config.h
+
+// Configuration header definitions
+
+#define PROJECT_NAME "AGENDA"
+#define VERSION "1.0.0"
+#define AUTHOR "Olumendes"
+#define DATE_TIME "2026-04-17 14:03:55"
+#define DEBUG_ENABLED 1
+
+// Add additional configurations as necessary
