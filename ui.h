011283a74507b35c19a8c4526192657efa871f77
olumendes <123881011+olumendes@users.noreply.github.com>
2026-04-17 11:13:46 -0300

    Create ui.h for user interface header definitions

---
+++ ui.h
@@ -0,0 +1,13 @@
+# UI Header Definitions for AGENDA
+
+#ifndef UI_H
+#define UI_H
+
+#include <stdio.h>
+
+// Function declarations for display functions
+void display_main_menu();
+void display_sub_menu();
+void display_error_message(const char* message);
+
+#endif // UI_H
